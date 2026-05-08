c3b2df7d158f4f196921fd7aba28188247783f28
Daniele Cerrina <daniecerri@gmail.com>
2026-05-08 16:49:41 +0200

    Checkpoint lezione 08/05

---
+++ Lezioni/lezione_2026_05_08/variabili_base.py
@@ -0,0 +1,29 @@
+# Creiamo una variabile che si chiama nome e ci mettiamo dentro
+# il valore "Daniele"
+nome = "Daniele" # Diversa da "eleDani" e da "daniele"
+
+# Creiamo una variabile altezza e ci mettiamo il valore 1.75
+altezza_float = 1.75  # è un numero float
+altezza_str = "1.75"  # è una stringa
+# Queste due variabili NON sono uguali
+
+# Script per calcolare l'area di un triangolo
+base = 14.3
+altezza = 23
+
+area = base * altezza / 2
+print(area)
+
+paragrafo = """
+Qualcosa 
+Che tiene traccia
+Della formattazione
+
+"""
+print(paragrafo)
+
+nome = "Daciele"
+
+
+
+
