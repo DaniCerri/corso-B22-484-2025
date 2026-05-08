2b79c6583f458eba92548e78c23380fc4f05c541
Daniele Cerrina <daniecerri@gmail.com>
2026-05-08 20:10:19 +0200

    Fine lezione 08/05

--- Lezioni/lezione_2026_05_08/esercizio3.py
+++ Lezioni/lezione_2026_05_08/esercizio3.py
@@ -21,3 +21,26 @@ Calcoliamo:
 
 coeff_redditivita = input("Inserisci il coeff di redditività (N%): ")
 coeff_redditivita = int(coeff_redditivita) / 100
+
+coeff_inps = input("Inserisci il coeff di INPS (N%): ")
+coeff_inps = int(coeff_inps) / 100
+
+coeff_irpef = input("Inserisci il coeff di IRPEF (N%): ")
+coeff_irpef = int(coeff_irpef) / 100
+
+fatturato = input("Inserisci il fatturato (N€): ")
+fatturato = float(fatturato)
+
+n_fatture = input("Inserisci il numero di fatture (N): ")
+n_fatture = int(n_fatture)
+
+da_pagare_inps = fatturato * coeff_redditivita * coeff_inps
+da_pagare_irpef = fatturato * coeff_redditivita * coeff_irpef
+
+fatt_media = fatturato / n_fatture
+
+# netto = fatturato - (da_pagare_inps + da_pagare_irpef)
+# perc_netto = netto / fatturato
+
+# perc_netto = 1 - coeff_redditivita * coeff_irpef + coeff_redditivita * coeff_inps
+perc_netto = 1 - (coeff_redditivita * (coeff_irpef + coeff_inps))
