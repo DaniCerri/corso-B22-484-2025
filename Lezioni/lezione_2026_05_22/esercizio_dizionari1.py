c953dc29c6a0f8991348ddc5e0eff41be0527932
Daniele Cerrina <daniecerri@gmail.com>
2026-05-22 15:24:34 +0200

    Checkpoint 22/05

---
+++ Lezioni/lezione_2026_05_22/esercizio_dizionari1.py
@@ -0,0 +1,16 @@
+"""
+Facciamo un dizionario con dentro 3 coppie di chiave-valore così
+strutturate:
+
+Letture di temperatura
+Torino: 10, 20.2, 12.2, 10, 24, 20, 10.9
+Roma: 14, 24.2, 16.2, 16, 28, 12
+Palermo: 21, 20.2, 17.8, 25, 27
+
+Calcoliamo e stampiamo per ogni città la temperatura media
+Città: N °C
+
+Poi calcoliamo la media totale e la stampiamo
+Quest'ultima media NON è (media_T + media_R + media_P) / 3
+
+"""
