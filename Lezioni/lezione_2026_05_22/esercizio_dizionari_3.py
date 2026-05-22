4ebaa945a46aa4e694bac4df12bcc752a5b3ce72
Daniele Cerrina <daniecerri@gmail.com>
2026-05-22 20:22:17 +0200

    Fine lezione 22/05

--- Lezioni/lezione_2026_05_22/esercizio_dizionari_3.py
+++ Lezioni/lezione_2026_05_22/esercizio_dizionari_3.py
@@ -8,7 +8,29 @@ Non facciamolo case sensitive -> A = a etc
 stringa = "Ciao come state? Tutto bene?"
 dizionario_caratteri = {}
 
-# BONUS: cercate come ottenere un valore di default dal dizionario
+# Per ogni carattere nella stringa
+for carattere in stringa.lower():
+    # Vogliamo fare in modo che il dizionario abbia la coppia carattere: conteggio
+    # Ci sono due possibilità:
+    #    1. abbiamo nel dizionario già la coppia carattere: conteggio
+    #        -> Vuol dire che eseguendo il for abbiamo già incontrato almeno una volta il carattere
+    #        -> Dobbiamo incrementare di 1 il contatore
+    #    2. non abbiamo ancora visto questo carattere
+    #        -> Creiamo la nuova coppia nel dizionario
+    #        -> Il contatore va messo a 1 (la prima, e finora unica, volta in cui abbiamo trovato il carattere)
 
+    # Questa riga prova a prendere il valore nel dizionario che ha chiave il carattere attuale
+    # Se non lo trova, restituisce 0
+    conteggio = dizionario_caratteri.get(carattere, 0) + 1
+
+    # if carattere not in dizionario_caratteri.keys():
+    #     conteggio = 0 + 1
+    # else:
+    #     conteggio = dizionario_caratteri[carattere] + 1
+
+    # con la riga sotto creiamo la coppia carattere conteggio se non c'era, altrimenti la aggiorniamo
+    dizionario_caratteri[carattere] = conteggio
 
 # Vogliamo ottenere un dizionario che avrà come chiavi i caratteri e come valori i conteggio
+print(dizionario_caratteri)
+
