8b7a416dba6a574ba4ae9d348a826492cee8b78d
Daniele Cerrina <daniecerri@gmail.com>
2026-05-22 19:27:24 +0200

    Checkpoint 22/05

---
+++ Lezioni/lezione_2026_05_22/esercizio_dizionari_3.py
@@ -0,0 +1,14 @@
+"""
+Prendiamo un testo (una stringa) e facciamo un dizionario
+che conti ogni carattere quante volte compare
+
+Non facciamolo case sensitive -> A = a etc
+"""
+
+stringa = "Ciao come state? Tutto bene?"
+dizionario_caratteri = {}
+
+# BONUS: cercate come ottenere un valore di default dal dizionario
+
+
+# Vogliamo ottenere un dizionario che avrà come chiavi i caratteri e come valori i conteggio
