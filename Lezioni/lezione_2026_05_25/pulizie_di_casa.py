25cd4d1218f3ed65b4074ae1a59753c44c6e3a82
Daniele Cerrina <daniecerri@gmail.com>
2026-05-25 20:23:22 +0200

    Fine lezione 25/05

--- Lezioni/lezione_2026_05_25/pulizie_di_casa.py
+++ Lezioni/lezione_2026_05_25/pulizie_di_casa.py
@@ -14,3 +14,31 @@ diz_pulizie = {
     "Marco": {"Cucina": 3, "Bagno": 4, "Salotto": 5},
 }
 
+pulizie_totali_persona = {}
+pulizie_totali_stanza = {}
+
+for persona, pulizie in diz_pulizie.items():
+    # Riempiamo il primo dizionario
+    somma = sum(pulizie.values())
+    pulizie_totali_persona[persona] = somma
+    print(f"  * {persona} ha pulito {somma} volte")
+
+    # Riempiamo il secondo dizionario
+    for stanza, n_volte in pulizie.items():
+        contatore = pulizie_totali_stanza.get(stanza, 0) + n_volte
+        pulizie_totali_stanza[stanza] = contatore
+
+print(pulizie_totali_stanza)
+
+n_prossime_pulizie = 30
+
+for i in range(n_prossime_pulizie):
+    peggior_pulitore = min(pulizie_totali_persona, key=pulizie_totali_persona.get)
+    stanza_meno_pulita = min(pulizie_totali_stanza, key=pulizie_totali_stanza.get)
+
+    print(f"{i + 1}. {peggior_pulitore} deve pulire {stanza_meno_pulita}")
+
+    pulizie_totali_persona[peggior_pulitore] += 1
+    pulizie_totali_stanza[stanza_meno_pulita] += 1
+
+
