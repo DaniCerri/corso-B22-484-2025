37d0f07731564ccb38f43baae6fe60f8613af0fe
Daniele Cerrina <daniecerri@gmail.com>
2026-06-08 16:50:17 +0200

    Esercizi sulle funzioni

---
+++ Lezioni/lezione_2026_06_08/esercizi_funzioni.py
@@ -0,0 +1,24 @@
+"""
+Facciamo una funzione aabb
+che presa una lista di numeri per ognuno
+ * se è multiplo di 3 stampa "aa"
+ * se è multiplo di 5 stampa "bb"
+ * se è multiplo di 15 stampa "aabb"
+"""
+
+"""
+Facciamo una funzione che data una lista ordinata (crescente) di numeri e 
+un target stampa True se esistono due numeri nella lista che sommati
+danno il target
+"""
+
+"""
+Facciamo una funzione che dato un numero n calcola l'n-simo numero 
+di fibonacci
+0 1 1 2 3 5 8 
+numero_0 = 0
+numero_1 = 1
+numero_n = numero_(n-1) + numero_(n-2)
+"""
+
+
