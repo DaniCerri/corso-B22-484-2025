2ae03e4950e8bc582e10d2aa0f94fc0276474f25
Daniele Cerrina <daniecerri@gmail.com>
2026-06-15 18:02:07 +0200

    Checkpoint lezione 15/06

--- Lezioni/lezione_2026_06_15/frazione.py
+++ Lezioni/lezione_2026_06_15/frazione.py
@@ -10,3 +10,33 @@ class Frazione:
         Se sono entrambi 0 -> Restituisce "nan"
         :return: valutazione della funzione
         """
+        if self.denominatore == 0 and self.numeratore == 0:
+            return "nan"
+        elif self.denominatore == 0:
+            segno = "+"
+            if self.numeratore < 0:
+                segno = "-"
+
+            return f"{segno} inf"
+
+        return self.numeratore / self.denominatore
+
+    # Andiamo a definire il comportamento che questa classe
+    # ha con l'operatore somma
+    def __add__(self, other):
+        # Regole da rispettare:
+        # 1. La somma di due infiniti uguali dà inf di segno concorde
+        #   --> + inf + + inf = + inf
+        #   --> - inf + - inf = - inf
+        # 2. La somma di due infiniti discordi dà nan
+        # 3. Numero + (+/-) inf dà (+/-) inf
+        # 4. se c'è almeno un nan dà nan
+        # 5. se non c'è una regola di sopra che si applica,
+        #    basta fare la somma classica
+
+
+
+
+
+
+
