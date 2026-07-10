b138a6143c3d9187d575e8381867062a4754a6ed
Daniele Cerrina <daniecerri@gmail.com>
2026-07-10 19:09:05 +0200

    Correzione query1

--- Lezioni/lezione_2026_07_10/query1.py
+++ Lezioni/lezione_2026_07_10/query1.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import db_connection as db
+
 engine = db.crea_engine(True)
+
 query = """
 SELECT 
     o.id AS ordine_id,
@@ -17,12 +19,17 @@ JOIN categorie c ON a.categoria_id = c.id;
 """
 
 df = pd.read_sql(query, engine)
-print(df)
+# print(df)
+# print(df.columns)
 
 # =========================================================
 # Domanda 1
 # Filtra solo le righe della categoria 'Elettronica' e stampale.
 # =========================================================
+filtro1 = df['categoria'] == "Elettronica"
+elettronica = df[filtro1]
+print("DOMANDA 1")
+print(elettronica[['ordine_id', 'data_ordine', 'categoria']])
 
 
 # =========================================================
@@ -30,11 +37,15 @@ print(df)
 # Aggiungi al DataFrame una colonna 'sconto' calcolata come
 # prezzo_listino - prezzo_vendita.
 # =========================================================
-
+df['sconto'] = df['prezzo_listino'] - df['prezzo_vendita']
+print("DOMANDA 2")
+print(df[['prezzo_listino', 'prezzo_vendita', 'sconto']])
 
 # =========================================================
 # Domanda 3
 # Trova la riga con il prezzo_vendita piu' alto e stampa a quale
 # categoria appartiene.
 # =========================================================
-
+filtro2 = df['prezzo_vendita'] == df['prezzo_vendita'].max()
+print("DOMANDA 3")
+print(df[filtro2][['ordine_id', 'categoria', 'prezzo_vendita']])
