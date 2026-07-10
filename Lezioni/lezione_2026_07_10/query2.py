bbc86d39b3b5c99d2e88b37953ff888b68d7c2dd
Daniele Cerrina <daniecerri@gmail.com>
2026-07-10 19:53:24 +0200

    Correzione query2

--- Lezioni/lezione_2026_07_10/query2.py
+++ Lezioni/lezione_2026_07_10/query2.py
@@ -26,18 +26,30 @@ print(df)
 # Domanda 1
 # Filtra solo le righe degli impiegati con ruolo 'Manager' e stampale.
 # =========================================================
-
+filtro1 = df['ruolo'] == 'Manager'
+print("DOMANDA 1")
+print(df[filtro1][['impiegato_id', 'ruolo']])
 
 # =========================================================
 # Domanda 2
 # Trova l'impiegato con lo stipendio piu' alto e stampa nome, cognome
 # e il nome dell'ufficio in cui lavora.
 # =========================================================
-
+filtro2 = df['stipendio'] == df['stipendio'].max()
+print("DOMANDA 2")
+print(df[filtro2][['nome', 'cognome', 'nome_ufficio', 'stipendio']])
 
 # =========================================================
 # Domanda 3
 # Filtra solo le righe degli impiegati che lavorano in un ufficio
 # situato in Lombardia.
 # =========================================================
+filtro3 = df['regione_ufficio'] == 'Lombardia'
+print("DOMANDA 3")
+print(df[filtro3][['regione_ufficio', 'nome', 'cognome']])
 
+# TODO: stampare tutti gli impiegati che hanno stipendio sopra la media e che
+#  hanno l'ufficio in Lombardia
+filtro4 = df['stipendio'] >= df['stipendio'].mean()
+print("BONUS")
+print(df[filtro3 & filtro4][['regione_ufficio', 'stipendio', 'nome', 'cognome']])
